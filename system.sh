aa6261799956527f13b3da0e760da6896ad3ab23
Shoko Sugito <SugiShio@users.noreply.github.com>
2023-08-22 16:12:06 +0900

    Create system.sh

---
+++ system.sh
@@ -0,0 +1,11 @@
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture 0
+defaults write "Apple Global Domain" InitialKeyRepeat 25
+defaults write "Apple Global Domain" KeyRepeat 2
+defaults write "Apple Global Domain" "com.apple.trackpad.scaling" 2
+defaults write "Apple Global Domain" "KB_DoubleQuoteOption" "\\U201cabc\\U201d";
+defaults write "Apple Global Domain" "KB_SingleQuoteOption" "\\U2018abc\\U2019";
+defaults write "Apple Global Domain" "KB_SpellingLanguage" '{"KB_SpellingLanguageIsAutomatic" = 1;}'
+defaults write "com.apple.HIToolbox" AppleFnUsageType 0
