04398cdb9d10fc09335f4cc9b1fc70752d311167
Shoko Sugito <SugiShio@users.noreply.github.com>
2023-08-22 14:50:01 +0900

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -6,13 +6,8 @@ nodebrew install-binary stable
 nodebrew use stable
 
 # パスを通す
-echo export PATH=$HOME/.nodebrew/current/bin:$PATH >> ~/.bash_profile
-source ~/.bash_profile
-
-npm i -g json-server
-
-brew install yarn
-brew install heroku
+echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.zprofile
+source ~/.zprofile
 
 brew install rbenv ruby-build
 rbenv install 2.3.0
